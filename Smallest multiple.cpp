131fae2fa473a9155f3b5f5e81e48d8ea15e6f4c
Frederico02 <81171707+Frederico02@users.noreply.github.com>
2022-09-25 22:01:34 -0300

    Add files via upload

---
+++ Smallest multiple.cpp
@@ -0,0 +1,12 @@
+using namespace std;
+int main()
+{
+	int i;
+	int t=0;
+	for (i=1;;i++)
+	{
+		if ((i%11)==0 && (i%12)==0&&(i%13)==0 && (i%14)==0 && (i%15)==0 && (i%16)==0 && (i%17)==0 && (i%18)==0 && (i%19)==0 && (i%20)==0) break;
+	}
+	 cout<<i;
+return 0;
+}
